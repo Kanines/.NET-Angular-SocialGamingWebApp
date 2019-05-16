428ae6998c73d23d1b17623b8dc4dabb14397f89
Kanines <kamilbielecki10@gmail.com>
2019-05-16 13:15:17 +0200

    Initial Commit

---
+++ KaniWebApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using KaniWebApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaniWebApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ KaniWebApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace KaniWebApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }      
+    }
+}
