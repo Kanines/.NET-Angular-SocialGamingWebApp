5769cf40c5d03c8b8a8a6467616ba0588678360f
Kanines <kamilbielecki10@gmail.com>
2019-05-27 00:28:38 +0200

    Added route resolvers & NgxGallery

--- KaniWebApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ KaniWebApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAction, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,19 +12,40 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.laodUser();
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '400px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
   }
 
-  laodUser() {
-    this.userService.getUser(this.route.snapshot.params['id']).subscribe((user: User) => {
-      this.user = user;
-    }, error => {
-      this.alertify.error(error);
-    });
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.images.length; i++) {
+      imageUrls.push({
+        small: this.user.images[i].url,
+        medium: this.user.images[i].url,
+        large: this.user.images[i].url,
+        description: this.user.images[i].description
+      })
+    }
+    return imageUrls;
   }
 
 }
--- KaniWebApp-SPA/src/app/routes.ts
+++ KaniWebApp-SPA/src/app/routes.ts
@@ -1,10 +1,13 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { FriendListComponent } from './friend-list/friend-list.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -13,8 +16,8 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent },
-            { path: 'members/:id', component: MemberDetailComponent },
+            { path: 'members', component: MemberListComponent, resolve: { users: MemberListResolver} },
+            { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
             { path: 'messages', component: MessagesComponent },
             { path: 'friends', component: FriendListComponent }
         ]
