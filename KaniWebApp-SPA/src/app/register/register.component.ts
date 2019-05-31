55e2bf4456c38ab4deca8e08f877239ea49e660d
Kanines <kamilbielecki10@gmail.com>
2019-05-31 23:30:21 +0200

    Added reactive validation & expanded Registration form

--- KaniWebApp-SPA/src/app/register/register.component.ts
+++ KaniWebApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,38 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alerify: AlertifyService) { }
+  constructor(private authService: AuthService, private alerify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(24)]],
+      confirmPassword: ['', Validators.required],
+      gender: ['dunno'],
+      nickname: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { 'mismatch': true };
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alerify.success('Registration successful');
-    }, error => {
-      this.alerify.error(error);
-    })
+    //   this.authService.register(this.model).subscribe(() => {
+    //     this.alerify.success('Registration successful');
+    //   }, error => {
+    //     this.alerify.error(error);
+    //   })
+    console.log(this.registerForm.value);
   }
 
   cancel() {
