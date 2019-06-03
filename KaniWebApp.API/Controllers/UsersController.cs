fdda47c9bcc234d38fa06540047b799b091e6e8c
Kanines <kamilbielecki10@gmail.com>
2019-06-04 00:26:40 +0200

    Added lastActive ActionFilter & backend Pagination

--- KaniWebApp.API/Controllers/UsersController.cs
+++ KaniWebApp.API/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using AutoMapper;
 using KaniWebApp.API.Data;
 using KaniWebApp.API.Dtos;
+using KaniWebApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KaniWebApp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
@@ -25,16 +27,18 @@ namespace KaniWebApp.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var users = await _repo.GetUsers(userParams);
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(usersToReturn);
         }
 
-        [HttpGet("{id}", Name="GetUser")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
@@ -49,12 +53,12 @@ namespace KaniWebApp.API.Controllers
         {
             if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
-            
+
             var userFromRepo = await _repo.GetUser(id);
 
             _mapper.Map(userForUpdateDto, userFromRepo);
 
-            if(await _repo.SaveAll())
+            if (await _repo.SaveAll())
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
--- KaniWebApp.API/Data/IWebAppRepository.cs
+++ KaniWebApp.API/Data/IWebAppRepository.cs
@@ -1,17 +1,18 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using KaniWebApp.API.Helpers;
 using KaniWebApp.API.Models;
 
 namespace KaniWebApp.API.Data
 {
     public interface IWebAppRepository
     {
-         void Add<T>(T entity) where T: class;
-         void Delete<T>(T entity) where T: class;
-         Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
-         Task<User> GetUser(int id);
-         Task<Image> GetImage (int id);
-         Task<Image> GetMainImageForUser(int userId);
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<PagedList<User>> GetUsers(UserParams UserParams);
+        Task<User> GetUser(int id);
+        Task<Image> GetImage(int id);
+        Task<Image> GetMainImageForUser(int userId);
     }
 }
